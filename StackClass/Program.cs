23a84ddcac1bcf4019b51bdc3425732cdaf175d1
Trần Quan Thiện <tranquthien@gmail.com>
2015-03-12 20:42:18 +0700

    Revert "Them ham Push"
    This reverts commit 6cdf44cbba855bdb64b47777b5e276477a364979.

--- StackClass/Program.cs
+++ StackClass/Program.cs
@@ -10,39 +10,6 @@ namespace StackClass
     {
         static void Main(string[] args)
         {
-            int capaticy;
-            Console.WriteLine("Enter capacity of Stack");
-            capaticy = int.Parse(Console.ReadLine());
-            MyStack<string> stack = new MyStack<string>(capaticy);
-            //chon chon chu nang 
-            Console.WriteLine("1.Push");
-
-            int choise;
-            choise = int.Parse(Console.ReadLine());
-            switch (choise)
-            {
-                case 1:
-                    {
-                        Console.WriteLine("Enter string to Stack");
-                        string temp = Console.ReadLine();
-                        int result = stack.Push(temp);
-                        if (result != -1)
-                        {
-                            Console.WriteLine("Element pushed into Stack");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Stack overflow");
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine("You have entered wrong choise");
-                        break;
-                    }
-            }
-            Console.ReadLine();
         }
     }
 }
