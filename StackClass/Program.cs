7a3b19913347dd3675f2ebae99d9215223bbe886
Trần Quan Thiện <tranquthien@gmail.com>
2015-03-13 21:02:47 +0700

    Revert "Revert "Add 2 method""
    This reverts commit 5318be0e928feb12a488c35cc40555144af2ae12.

--- StackClass/Program.cs
+++ StackClass/Program.cs
@@ -12,10 +12,18 @@ namespace StackClass
         {
             int capaticy;
             Console.WriteLine("Enter capacity of Stack");
-            capaticy = int.Parse(Console.ReadLine());
+        back: capaticy = int.Parse(Console.ReadLine());
+            if (capaticy <= 0)
+            {
+                Console.WriteLine("Sorry, you enter a different value for capacity of Stack");
+                goto back;
+            }
             MyStack<string> stack = new MyStack<string>(capaticy);
             //chon chon chu nang 
             Console.WriteLine("1.Push");
+        choiseMethod:
+            Console.WriteLine("2.Pop");
+            Console.WriteLine("3.Peep");
 
             int choise;
             choise = int.Parse(Console.ReadLine());
@@ -24,15 +32,46 @@ namespace StackClass
                 case 1:
                     {
                         Console.WriteLine("Enter string to Stack");
-                        string temp = Console.ReadLine();
-                        int result = stack.Push(temp);
-                        if (result != -1)
+                        while (capaticy != 0)
                         {
-                            Console.WriteLine("Element pushed into Stack");
+                            string temp = Console.ReadLine();
+                            stack.Push(temp);
+                            capaticy--;
+                            if (capaticy == 0)
+                            {
+                                Console.WriteLine("Element pushed into Stack");
+                                goto choiseMethod;
+                            }
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        string result = stack.Pop();
+                        if (result != null)
+                        {
+                            Console.WriteLine("Delete element: " + result);
+                            goto choiseMethod;
                         }
                         else
                         {
-                            Console.WriteLine("Stack overflow");
+                            Console.WriteLine("Stack unoverflow");
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Enter position of Element to Pop");
+                        int position = int.Parse(Console.ReadLine());
+                        string result = stack.Peep(position);
+                        if (result != null)
+                        {
+                            Console.WriteLine("Element at position" + position + "is" + result);
+                            goto choiseMethod;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Entered element is out of Stack element");
                         }
                         break;
                     }
